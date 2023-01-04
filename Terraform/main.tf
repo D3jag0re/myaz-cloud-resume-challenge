bd3e6bd552a86331d59286b8ebdf62f7c6faf71b
D3jag0re <mbrinkwo@gmail.com>
2023-01-04 14:28:31 -0500

    Added to ignore

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,3 +12,20 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "prod" {
+  name     = var.rgname
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.saname
+  resource_group_name      = azurerm_resource_group.prod.name
+  location                 = azurerm_resource_group.prod.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "production"
+  }
+}
+
